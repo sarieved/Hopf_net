6c5dc2ef4ca5489bbe749272acad5e57e0d71165
Эдуард Сариев < >
2018-01-09 20:13:14 +0300

    Добавьте файлы проекта.

---
+++ N2/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace N2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.load_im = new System.Windows.Forms.Button();
+            this.add_noise = new System.Windows.Forms.Button();
+            this.teach = new System.Windows.Forms.Button();
+            this.determ = new System.Windows.Forms.Button();
+            this.class_num_lab = new System.Windows.Forms.Label();
+            this.class_x = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(250, 239);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // load_im
+            // 
+            this.load_im.Location = new System.Drawing.Point(312, 42);
+            this.load_im.Name = "load_im";
+            this.load_im.Size = new System.Drawing.Size(97, 25);
+            this.load_im.TabIndex = 1;
+            this.load_im.Text = "Load Image";
+            this.load_im.UseVisualStyleBackColor = true;
+            this.load_im.Click += new System.EventHandler(this.load_im_Click);
+            // 
+            // add_noise
+            // 
+            this.add_noise.Location = new System.Drawing.Point(275, 73);
+            this.add_noise.Name = "add_noise";
+            this.add_noise.Size = new System.Drawing.Size(96, 24);
+            this.add_noise.TabIndex = 2;
+            this.add_noise.Text = "Add Noise";
+            this.add_noise.UseVisualStyleBackColor = true;
+            this.add_noise.Click += new System.EventHandler(this.add_noise_Click);
+            // 
+            // teach
+            // 
+            this.teach.Location = new System.Drawing.Point(312, 12);
+            this.teach.Name = "teach";
+            this.teach.Size = new System.Drawing.Size(97, 24);
+            this.teach.TabIndex = 3;
+            this.teach.Text = "Samples";
+            this.teach.UseVisualStyleBackColor = true;
+            this.teach.Click += new System.EventHandler(this.samples_Click);
+            // 
+            // determ
+            // 
+            this.determ.Location = new System.Drawing.Point(275, 103);
+            this.determ.Name = "determ";
+            this.determ.Size = new System.Drawing.Size(96, 26);
+            this.determ.TabIndex = 4;
+            this.determ.Text = "Determinate";
+            this.determ.UseVisualStyleBackColor = true;
+            this.determ.Click += new System.EventHandler(this.determ_Click);
+            // 
+            // class_num_lab
+            // 
+            this.class_num_lab.AutoSize = true;
+            this.class_num_lab.Location = new System.Drawing.Point(324, 141);
+            this.class_num_lab.Name = "class_num_lab";
+            this.class_num_lab.Size = new System.Drawing.Size(38, 13);
+            this.class_num_lab.TabIndex = 5;
+            this.class_num_lab.Text = "Class: ";
+            // 
+            // class_x
+            // 
+            this.class_x.AutoSize = true;
+            this.class_x.Location = new System.Drawing.Point(359, 141);
+            this.class_x.Name = "class_x";
+            this.class_x.Size = new System.Drawing.Size(33, 13);
+            this.class_x.TabIndex = 6;
+            this.class_x.Text = "None";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox2.Location = new System.Drawing.Point(309, 157);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(377, 110);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "e:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(378, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(15, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "%";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(393, 77);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(46, 20);
+            this.numericUpDown1.TabIndex = 10;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.DecimalPlaces = 3;
+            this.numericUpDown2.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            196608});
+            this.numericUpDown2.Location = new System.Drawing.Point(393, 108);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(46, 20);
+            this.numericUpDown2.TabIndex = 11;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(449, 275);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.class_x);
+            this.Controls.Add(this.class_num_lab);
+            this.Controls.Add(this.determ);
+            this.Controls.Add(this.teach);
+            this.Controls.Add(this.add_noise);
+            this.Controls.Add(this.load_im);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Hopfield network";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button load_im;
+        private System.Windows.Forms.Button add_noise;
+        private System.Windows.Forms.Button teach;
+        private System.Windows.Forms.Button determ;
+        private System.Windows.Forms.Label class_num_lab;
+        private System.Windows.Forms.Label class_x;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ N2/Form1.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace N2
+{
+    public partial class Form1 : Form
+    {
+        List<List<int>> WM;
+        List<string> ns = new List<string>();
+        Graphics g1;
+        Graphics g2;
+        int Vsize;
+        List<List<int>> samples;
+        List<int> samp;
+        int[] init;
+        int[] dist;
+        string cur_fold;
+        string fname;
+
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox2.Image = new Bitmap(pictureBox2.Width, pictureBox2.Height);
+            g1 = Graphics.FromImage(pictureBox1.Image);
+            g2 = Graphics.FromImage(pictureBox2.Image);
+            g1.Clear(Color.White);
+            g2.Clear(Color.White);
+        }
+
+        double ham_dist(int[] a, int[] b, int n)
+        {
+            int sum = 0;
+            for (int i = 0; i < n; ++i)
+                sum += a[i] * b[i];
+            double dist = 1 / 2.0 * (n - sum);
+            return dist / n;
+        }
+
+        double ham_dist(List<int> a, List<int> b, int n)
+        {
+            int sum = 0;
+            for (int i = 0; i < n; ++i)
+                sum += a[i] * b[i];
+            double dist = 1 / 2.0 * (n - sum);
+            return dist / n;
+        }
+        
+        void hebb(int n, List<List<int>> x)
+        {
+            WM = new List<List<int>>();
+            for (int i = 0; i < Vsize; ++i)
+                WM.Add(new List<int>(Vsize));
+
+            for (int i = 0; i < n; ++i)
+                for (int j = 0; j < n; ++j)
+                {
+                    if (i == j)
+                        WM[i].Insert(j, 0);
+                    else
+                    {
+                        int sum = 0;
+                        int a = 0;
+                        int b = 0;
+                        for (int k = 0; k < x.Count; ++k)
+                        {
+                            a = x[k][j];
+
+                            b = x[k][i];
+                            sum += a * b;
+                        }
+                        WM[i].Insert(j, sum);
+                    }
+                }
+        }
+
+        int f_act(int x)
+        {
+            if (x > 0)
+                return 1;
+            else return -1;
+        }
+
+        void hopf(int[] v, double eps)
+        {
+            List<int> curr = new List<int>();
+            for (int i = 0; i < Vsize; ++i)
+                curr.Add(v[i]);
+            bool stop = false;
+            while (!stop)
+            {
+                List<int> next_y = new List<int>();
+                for (int j = 0; j < Vsize; ++j)
+                {
+                    int sum = 0;
+                    for (int i = 0; i < Vsize; ++i)
+                    {
+                        sum += WM[i][j] * curr[i];
+                    }
+                    if (sum <= 0)
+                        next_y.Add(-1);
+                    else
+                        next_y.Add(1);
+                }
+
+                double diff = ham_dist(curr, next_y, Vsize);
+                if (diff <= eps)
+                    stop = true;
+                curr = next_y;
+            }
+            Bitmap bm = new Bitmap(pictureBox2.Image);
+            int pictsz = Convert.ToInt32(Math.Sqrt((double)Vsize));
+            int l = 0;
+            for (int i = 0; i < pictsz; ++i)
+                for (int j = 0; j < pictsz; ++j)
+                {
+                    if (curr[l] == 1)
+                        bm.SetPixel(j, i, Color.White);
+                    else
+                        bm.SetPixel(j, i, Color.Black);
+                    ++l;
+                }
+            
+            pictureBox2.Image = bm;
+
+            int samp_ind = 0;
+            double min_Hd = double.MaxValue;
+            for (int i = 0; i < samples.Count; ++i)
+            {
+                double cHd = ham_dist(curr, samples[i], Vsize);
+                if (cHd < min_Hd)
+                {
+                    min_Hd = cHd;
+                    samp_ind = i;
+                }
+            }
+            class_x.Text = ns[samp_ind];
+        }
+
+        private void samples_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            cur_fold = folderBrowserDialog1.SelectedPath;
+            DirectoryInfo DI = new DirectoryInfo(cur_fold);
+            List<string> files = new List<string>();
+            foreach (FileInfo file in DI.GetFiles("*.txt"))
+            {
+                files.Add(Path.GetFileName(file.FullName));
+                ns.Add(Path.GetFileNameWithoutExtension(file.Name));
+            }
+
+            samples = new List<List<int>>();
+            for (int i = 0; i < files.Count; ++i)
+            {
+                samp = new List<int>();
+                using (StreamReader reader = new StreamReader(File.Open(cur_fold + "\\" + files[i], FileMode.Open)))
+                {
+                    string text;
+
+                    while ((text = reader.ReadLine()) != null)
+                    {
+                        string[] bits = text.Split(' ');
+                        for (int l = 0; l < bits.Length - 1; ++l)
+                        {
+                            int s;
+                            s = int.Parse(bits[l]);
+                            samp.Add(s);
+                        }
+                    }
+                    samples.Add(samp);
+                }
+            }
+            Vsize = samples[0].Count;
+            hebb(Vsize, samples);
+
+            MessageBox.Show("Done!", "Done", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    
+        private void load_im_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            fname = openFileDialog1.FileName;
+            init = new int[Vsize];
+            dist = new int[Vsize];
+            using (StreamReader reader = new StreamReader(File.Open(fname, FileMode.Open)))
+            {
+                string text;
+                int m = 0;
+                while ((text = reader.ReadLine()) != null)
+                {
+                    string[] bits = text.Split(' ');
+                    for (int l = 0; l < bits.Length - 1; ++l)
+                    {
+                        int s;
+                        s = int.Parse(bits[l]);
+                        init[m] = s;
+                        dist[m] = s;
+                        ++m;
+                    }
+                }
+            }
+
+            int pictsz = Convert.ToInt32(Math.Sqrt((double)Vsize));
+            int k = 0;
+            Bitmap bm = new Bitmap(pictureBox1.Image);
+            for (int i = 0; i < pictsz; ++i)
+                for (int j = 0; j < pictsz; ++j)
+                {
+
+                    if (init[k] == 1)
+                        bm.SetPixel(j, i, Color.White);
+                    else
+                        bm.SetPixel(j, i, Color.Black);
+                    ++k;
+                }
+            pictureBox1.Image = bm;
+        }
+
+        private void add_noise_Click(object sender, EventArgs e)
+        {
+            int perc = Convert.ToInt32(numericUpDown1.Value);
+            int count = Convert.ToInt32(perc / 100.0 * Vsize);
+            Random R = new Random();
+            for (int i = 0; i < Vsize; ++i)
+                dist[i] = init[i];
+            while (count > 0)
+            {
+                int index = R.Next(0, Vsize);
+                if (init[index] == -1)
+                    dist[index] = 1;
+                else dist[index] = -1;
+                count--;
+            }
+            int pictsz = Convert.ToInt32(Math.Sqrt((double)Vsize));
+            int k = 0;
+            Bitmap bm = new Bitmap(pictureBox1.Image);
+            for (int i = 0; i < pictsz; ++i)
+                for (int j = 0; j < pictsz; ++j)
+                {
+                    if (dist[k] == 1)
+                        bm.SetPixel(j, i, Color.White);
+                    else
+                        bm.SetPixel(j, i, Color.Black);
+                    ++k;
+                }
+            pictureBox1.Image = bm;
+        }
+
+        private void determ_Click(object sender, EventArgs e)
+        {
+            double ep = Convert.ToDouble(numericUpDown2.Value);
+            hopf(dist, ep);
+        }
+    }
+}
